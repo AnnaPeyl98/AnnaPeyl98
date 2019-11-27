c7836e1aac0a258e6737f438d6944633c9645bf8
Peyl Anna <annapeyl@gmail.com>
2019-11-27 14:12:32 +0600

    added test for file writer

---
+++ src/test/java/it/sevenbits/writer/FileWriterTest.java
@@ -0,0 +1,34 @@
+package it.sevenbits.writer;
+
+import it.sevenbits.reader.implementation.FileReader;
+import it.sevenbits.writer.implementation.FileWriter;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class FileWriterTest {
+    private FileWriter fileWriter;
+    private FileReader fileReader;
+
+    @Before
+    public void init() {
+        fileWriter = mock(FileWriter.class);
+        fileReader = mock(FileReader.class);
+    }
+
+    @Test
+    public void testWriteChar() throws WriterException{
+        doAnswer(invocation -> {
+            when(fileReader.read()).thenReturn('f');
+            assertEquals('f', fileReader.read());
+            return null;
+        }).when(fileWriter).write('f');
+        fileWriter.write('f');
+    }
+
+}
