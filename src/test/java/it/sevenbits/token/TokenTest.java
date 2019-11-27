6180ce1599837b8ec9c78763599e2a200ff3acba
Peyl Anna <annapeyl@gmail.com>
2019-11-27 14:14:58 +0600

    added tests for token

---
+++ src/test/java/it/sevenbits/token/TokenTest.java
@@ -0,0 +1,42 @@
+package it.sevenbits.token;
+
+import it.sevenbits.lexer.LexerException;
+import it.sevenbits.token.implementation.Token;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TokenTest {
+    private Token token;
+    @Test
+    public void testWithMockGetName(){
+        String name = "SEMICOLON";
+        token = mock(Token.class);
+        when(token.getName()).thenReturn(name);
+        String resultName = token.getName();
+        assertEquals(name, resultName);
+    }
+    @Test
+    public void testWithMockGetLexeme(){
+        String lexeme = ";";
+        token = mock(Token.class);
+        when(token.getLexeme()).thenReturn(lexeme);
+        String resultName = token.getLexeme();
+        assertEquals(lexeme, resultName);
+    }
+
+    @Test
+    public void testGetName(){
+        token = new Token("SEMICOLON",";");
+        String name = "SEMICOLON";
+        assertEquals(name, token.getName());
+    }
+    @Test
+    public void testGetLexeme(){
+        token = new Token("SEMICOLON",";");
+        String name = ";";
+        assertEquals(name, token.getLexeme());
+    }
+}
